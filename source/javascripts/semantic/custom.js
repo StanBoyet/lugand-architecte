a7738c806bf35c775e4e1020fe8a3ad7fe7f8122
Stanislas Boyet <stanislas@boyet.me>
2017-01-22 17:02:29 +0100

    contact page

--- source/javascripts/semantic/custom.js
+++ source/javascripts/semantic/custom.js
@@ -320,7 +320,7 @@
 		 * Google Map
 		/* ---------------------------------------------- */
 
-		var mapLocation = new google.maps.LatLng(34.031428,-118.2071542,17);
+		var mapLocation = new google.maps.LatLng(48.6123069,-2.0511951);
 
 		var $mapis = $('#map');
 
@@ -339,7 +339,7 @@
 				styles: [{"featureType":"all","elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#000000"},{"lightness":40}]},{"featureType":"all","elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#000000"},{"lightness":16}]},{"featureType":"all","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":17},{"weight":1.2}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":21}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":18}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":16}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":19}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":17}]}]
 			});
 
-			var image = new google.maps.MarkerImage('assets/images/map-icon.png',
+			var image = new google.maps.MarkerImage('images/semantic/map-icon.png',
 				new google.maps.Size(59, 65),
 				new google.maps.Point(0, 0),
 				new google.maps.Point(24, 42)
@@ -350,7 +350,7 @@
 				icon: image,
 				title: 'Rival',
 				infoWindow: {
-					content: '<p><strong>Semantic</strong><br/>121 Somewhere Ave, Suite 123<br/>P: (123) 456-7890<br/>Australia</p>'
+					content: '<p><strong>Lugand-Delomotte  Architecte</strong><br/>P.A DE L’HERMITAGE - 35780 LA RICHARDAIS<br/>BP 10244 - 35802 DINARD CEDEX<br/><p>'
 				}
 			});
 
